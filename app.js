3e7c5047b707951a20d0cd3d1ee1ac5f53496731
Paul Milbank <paulmilbank@gmail.com>
2017-09-03 20:56:31 +1200

    initial setup to get a listening express server.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const express = require('express')
+const { appConfig, dbConfig } = require('./config/devSettings.json')
+
+const app = express()
+
+app.listen(appConfig.port, () => {
+  console.log(`Express listening on port ${appConfig.port}`)
+})
