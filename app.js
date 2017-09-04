44bc146bb43b8a4e603d94df58462994eb93ef29
Paul Milbank <paulmilbank@gmail.com>
2017-09-04 22:09:41 +1200

    log out the JSON we post to the / route to the console

--- app.js
+++ app.js
@@ -2,8 +2,20 @@
 
 const express = require('express')
 const { appConfig, dbConfig } = require('./config/devSettings.json')
+const bodyParser = require('body-parser').json
 
 const app = express()
+
+app.use(bodyParser())
+
+app.use('/', (req, res, next) => {
+  if (req.body) {
+    console.log(req.body)
+  } else {
+    console.log('no body to parse')
+  }
+  next()
+})
 
 app.listen(appConfig.port, () => {
   console.log(`Express listening on port ${appConfig.port}`)
