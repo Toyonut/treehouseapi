f6a6ed7d49ab4423241b07dc109322bf28817227
Paul Milbank <paulmilbank@gmail.com>
2017-09-05 22:30:52 +1200

    generate 404 error and return a json error object.

--- app.js
+++ app.js
@@ -9,10 +9,27 @@ const logger = require('morgan')
 const app = express()
 
 app.use(logger('dev'))
-
 app.use(bodyParser())
 
+// Routes
 app.use('/questions', router)
+
+// Generate 404 error
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({
+    error: {
+      status: status,
+      message: err.message
+    }
+  })
+})
 
 app.listen(appConfig.port, () => {
   console.log(`Express listening on port ${appConfig.port}`)
