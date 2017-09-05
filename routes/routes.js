b13e81e4b3a5b9ca6357975bfe2315b1cc1ad4f2
Paul Milbank <paulmilbank@gmail.com>
2017-09-05 22:09:09 +1200

    bracket formatting

--- routes/routes.js
+++ routes/routes.js
@@ -4,64 +4,52 @@ const router = express.Router()
 
 // GET /questions
 router.get('/', (req, res, next) => {
-  res.status(200).json(
-    {
-      response: 'GET request'
-    }
-  )
+  res.status(200).json({
+    response: 'GET request'
+  })
 })
 
 // POST to /questions
 router.post('/', (req, res, next) => {
-  res.status(200).json(
-    {
-      response: 'POST request',
-      body: req.body
-    }
-  )
+  res.status(200).json({
+    response: 'POST request',
+    body: req.body
+  })
 })
 
 // just demonstrating query string handling
 router.get('/query', (req, res, next) => {
-  res.status(200).json(
-    {
-      response: 'GET query',
-      query: req.query
-    }
-  )
+  res.status(200).json({
+    response: 'GET query',
+    query: req.query
+  })
 })
 
 // GET /questions/:id
 router.get('/:qID', (req, res, next) => {
-  res.status(200).json(
-    {
-      response: 'GET id',
-      id: req.params.id
-    }
-  )
+  res.status(200).json({
+    response: 'GET id',
+    id: req.params.id
+  })
 })
 
 // POST an answer on /questions/:id/answers.
 router.post('/:qID/answers', (req, res, next) => {
-  res.status(200).json(
-    {
-      response: 'POST request to /answers/qID',
-      id: req.params.qID,
-      body: req.body
-    }
-  )
+  res.status(200).json({
+    response: 'POST request to /answers/qID',
+    id: req.params.qID,
+    body: req.body
+  })
 })
 
 // PUT to update answer with specific answer ID.
 router.put('/:qID/answers/:aID', (req, res, next) => {
-  res.status(200).json(
-    {
-      response: 'PUT request to /:qID/answers/:aID',
-      qID: req.params.qID,
-      aID: req.params.aID,
-      body: req.body
-    }
-  )
+  res.status(200).json({
+    response: 'PUT request to /:qID/answers/:aID',
+    qID: req.params.qID,
+    aID: req.params.aID,
+    body: req.body
+  })
 })
 
 module.exports = router
