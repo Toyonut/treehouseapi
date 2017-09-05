8bcdd0c11ebe9969c99e126a7837cc6fdabfe55b
Paul Milbank <paulmilbank@gmail.com>
2017-09-05 21:59:24 +1200

    tidy up, command and add answers POST route

--- routes/routes.js
+++ routes/routes.js
@@ -2,20 +2,54 @@
 const express = require('express')
 const router = express.Router()
 
+// GET /questions
 router.get('/', (req, res, next) => {
-  res.status(200).json({response: 'get request'})
+  res.status(200).json(
+    {
+      response: 'GET request'
+    }
+  )
 })
 
+// POST to /questions
 router.post('/', (req, res, next) => {
-  res.status(200).json({response: 'post request', body: req.body})
+  res.status(200).json(
+    {
+      response: 'POST request',
+      body: req.body
+    }
+  )
 })
 
+// just demonstrating query string handling
 router.get('/query', (req, res, next) => {
-  res.status(200).json({response: 'get query', query: req.query})
+  res.status(200).json(
+    {
+      response: 'GET query',
+      query: req.query
+    }
+  )
 })
 
+// GET /questions/:id
 router.get('/:id', (req, res, next) => {
-  res.status(200).json({response: 'get id', id: req.params.id})
+  res.status(200).json(
+    {
+      response: 'GET id',
+      id: req.params.id
+    }
+  )
+})
+
+// POST an answer on /questions/:id/answers
+router.post('/:id/answers', (req, res, next) => {
+  res.status(200).json(
+    {
+      response: 'POST request to /answers',
+      id: req.params.id,
+      body: req.body
+    }
+  )
 })
 
 module.exports = router
