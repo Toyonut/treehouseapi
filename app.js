251cefa331f2900c0f7be075059a9177543bc552
Paul Milbank <paulmilbank@gmail.com>
2017-09-07 19:26:45 +1200

    Initial setup for connection to MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 'use strict'
 
 const express = require('express')
-const { appConfig, dbConfig } = require('./config/devSettings.json')
+const { appConfig, dbConf } = require('./config/devSettings.json')
 const bodyParser = require('body-parser').json
 const router = require('./routes/routes')
 const logger = require('morgan')
+const mongoose = require('mongoose')
+
+const mongoUri = `mongodb://${dbConf.user}:${dbConf.password}@${dbConf.url}:${dbConf.port}/${dbConf.name}`
 
 const app = express()
 
 app.use(logger('dev'))
 app.use(bodyParser())
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(mongoUri, {
+  useMongoClient: true
+})
+.then(() => { console.log(`DBConnection to ${dbConf.url} successful`) })
+.catch((err) => { logger(err) })
 
 // Routes
 app.use('/questions', router)
